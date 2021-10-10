75bb73c42dd4db44a27fadade101155e65fe61f1
Sumit Lahiri <lahiri.phdworks@gmail.com>
2021-10-10 10:20:07 +0530

    Examples Updated, Tests removed

--- examples/cfggen.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-opt -dot-cfg-only -cfg-func-name=main test$1/program$1.annotate.txt -disable-output -enable-new-pm=0
-mv .main.dot test$1/iblocks$1.dot
-
-opt -dot-cfg  -cfg-func-name=main test$1/program$1.annotate.txt -disable-output -enable-new-pm=0 
-mv .main.dot test$1/cfg$1.dot
--- examples/compile.sh
+++ examples/compile.sh
@@ -6,10 +6,19 @@ then
     exit 1
 fi
 
+if [[ $1 == "10" ]];
+then
+    echo "Reservoir Example"
+    echo 
+    clang -I include example10.c -O1 -emit-llvm -S -o example10.llvmir
+    clang -I include -c -Xclang -emit-llvm -O1 example10.c -o test10/program10.bc
+else
+    clang++ -I include example$1.cpp -O1 -emit-llvm -S -o example$1.llvmir
+    clang++ -I include -c -Xclang -emit-llvm -O1 example$1.cpp -o test$1/program$1.bc
+fi 
+
 mkdir -p test$1
 
-clang++ example$1.cpp -O1 -emit-llvm -S -o example$1.llvmir
-clang++ -c -Xclang -emit-llvm -O1 example$1.cpp -o test$1/program$1.bc
 opt -instnamer -mem2reg -enable-new-pm=0 test$1/program$1.bc -S -o test$1/program$1.mem2reg.ll
 
 cp test$1/program$1.mem2reg.ll test$1/program$1.annotate.txt
@@ -17,10 +26,11 @@ rm -rf test$1/program$1.bc
 
 echo "[Note] Annotate .annotate.txt file with @tau funcs and run cfggen.sh"
 
-opt -dot-cfg-only -cfg-func-name=main test$1/program$1.annotate.txt -disable-output -enable-new-pm=0
+opt -dot-cfg-only --view-cfg-only -cfg-func-name=main test$1/program$1.annotate.txt -disable-output -enable-new-pm=0
 mv .main.dot test$1/iblocks$1.dot
 
-opt -dot-cfg -cfg-func-name=main test$1/program$1.annotate.txt -disable-output -enable-new-pm=0 
+opt -dot-cfg --view-cfg-only -cfg-func-name=main test$1/program$1.annotate.txt -disable-output -enable-new-pm=0 
 mv .main.dot test$1/cfg$1.dot
 
+dot -T png test$1/iblocks$1.dot -o test$1/iblocks$1.png
 dot -T png test$1/cfg$1.dot -o test$1/cfg$1.png
