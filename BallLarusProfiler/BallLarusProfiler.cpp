1e4e0ed7d02ed28dcc45f4b38719036f232a1454
Abhay Mishra <mishraabhay9179913420@gmail.com>
2022-03-16 23:55:37 +0530

    WIP: Ball Larus Path Profiler

--- BallLarusProfiler/BallLarusProfiler.cpp
+++ BallLarusProfiler/BallLarusProfiler.cpp
@@ -3,35 +3,36 @@
 using namespace llvm;
 using namespace std;
 
-map<std::pair<const BasicBlock*,const BasicBlock*>, bool> isBackedge;
+map<std::pair<const BasicBlock*, const BasicBlock*>, bool> isBackedge;
 Graph BallLarusProfilerPass::getAbstractGraph(Function& F) {
-  BasicBlock* Exit = *po_begin(&F); // ? Exit will be the last block in post order iterator. Will it be inefficient?
+  BasicBlock* Exit = *po_begin(&F); // ? Exit will be the last block in post
+                                    // order iterator. Will it be inefficient?
   Graph AbstractGraph;
   SmallVector<std::pair<const BasicBlock*, const BasicBlock*>> result;
   FindFunctionBackedges(F, result); // backedges in this function
-  map<std::pair<const BasicBlock*,const BasicBlock*>, bool> isBackedge;
-  for(auto [from, to]: result) {
+  map<std::pair<const BasicBlock*, const BasicBlock*>, bool> isBackedge;
+  for (auto [from, to] : result) {
     isBackedge[make_pair(from, to)] = true;
   }
 
   uint backedge_ctr = 1;
-  for(auto& BB: F) {
-    for(auto Succ: successors(&BB)) {
-      if(!isBackedge[{&BB, Succ}]) {
+  for (auto& BB : F) {
+    for (auto Succ : successors(&BB)) {
+      if (!isBackedge[{&BB, Succ}]) {
         Edge nedge;
         nedge.from = &BB;
         nedge.to = Succ;
         nedge.backedge_number = 0; // ! not a backedge
         AbstractGraph.G[&BB].push_back(nedge);
-      }
-      else {
+      } else {
         Edge nedge1, nedge2;
         nedge1.from = &F.getEntryBlock();
         nedge1.to = Succ; // This will be the loop header
         nedge1.backedge_number = backedge_ctr++;
         nedge2.from = &BB;
         nedge2.to = Exit; // Exit Block
-        nedge2.backedge_number = nedge1.backedge_number; // Correspond to the same backedge
+        nedge2.backedge_number =
+            nedge1.backedge_number; // Correspond to the same backedge
         AbstractGraph.G[nedge1.from].push_back(nedge1);
         AbstractGraph.G[nedge2.from].push_back(nedge2);
       }
@@ -39,37 +40,66 @@ Graph BallLarusProfilerPass::getAbstractGraph(Function& F) {
   }
   return AbstractGraph;
 }
-void DFS(BasicBlock* BB)
-void DFSHelper() {
 
+vector<BasicBlock*> TopoSort(Graph& AG, BasicBlock* BBEntry) {
+  int cntr = 0;
+  map<BasicBlock*, int> visited, start, finish;
+  DFS(AG, BBEntry, visited, start, finish, cntr);
+  vector<BasicBlock*> BBList;
+  for (auto& [BB, outEdge] : AG.G)
+    BBList.push_back(BB);
+  // Sort in decreasing order of finishing time
+  std::sort(BBList.begin(), BBList.end(),
+            [&](BasicBlock* BB1, BasicBlock* BB2) {
+              return finish[BB1] > finish[BB2];
+            });
+  return BBList;
 }
-void BallLarusProfiler::getEdgeValues(Function &F, Graph &AG) {
-  map<BasicBlock*, int32_t> NumPaths;
 
-  for (auto it = po_begin(&F); it != po_end(&F);
-        ++it) { // Post Order Traversal--Reverse Topologiacl
+void DFS(Graph& AG, BasicBlock* BB, map<BasicBlock*, int>& visited,
+         map<BasicBlock*, int>& start, map<BasicBlock*, int>& finish,
+         int& cntr) {
+  visited[BB] = true;
+  start[BB] = cntr++;
+  for (auto outEdge : AG.G[BB]) {
+    if (visited[outEdge.to])
+      continue;
+    DFS(AG, outEdge.to, visited, start, finish, cntr);
+  }
+  finish[BB] = cntr++;
+}
+
+void BallLarusProfiler::getEdgeValues(Function& F, Graph& AG) {
+  map<BasicBlock*, int32_t> NumPaths;
+  vector<BasicBlock*> bbInPOrder = TopoSort(AG, F.getEntryBlock());
+
+  // Post Order Traversal--Reverse Topological
+  for (auto it = bbInPOrder.begin(); it != bbInPOrder.end(); ++it) {
     BasicBlock* BB = *it;
-    if (succ_empty(BB)) { // Leaf Node
+    if (AG.G[BB].empty()) { // Leaf Node
       NumPaths[BB] = 1;
     } else {
       NumPaths[BB] = 0;
-      for (auto Succ : successors(BB)) {
-        if(!isBackedge[{BB, Succ}]) {
-          AG.Edges[{BB, Succ}] // think what to do??
-        }
+      for (auto SuccEdge : AG.G[BB]) {
+        auto Succ = SuccEdge.to;
+        SuccEdge.val = NumPaths[BB];
         NumPaths[BB] = NumPaths[BB] + NumPaths[Succ];
       }
     }
   }
 }
 
+// ? Should we add the edge exit -> entry for mst
+
 // void BallLarusProfilerPass::convertToDAG(Function& F) {
-  // SmallVector<std::pair<const BasicBlock*, const BasicBlock*>> result;
-  // FindFunctionBackedges(F, result); // backedges in this function
+// SmallVector<std::pair<const BasicBlock*, const BasicBlock*>> result;
+// FindFunctionBackedges(F, result); // backedges in this function
 // }
 
 void BallLarusProfilerPass::getAnalysisUsage(AnalysisUsage& Info) {
-  Info.addRequired<UnifyFunctionExitNodesLegacyPass>(); // This is required as the paper requires exactly one exit block
+  Info.addRequired<
+      UnifyFunctionExitNodesLegacyPass>(); // This is required as the paper
+                                           // requires exactly one exit block
   // Used legacy pass for now, will change to new pass
 }
 
@@ -85,7 +115,6 @@ PreservedAnalyses BallLarusProfilerPass::run(Module& M,
     if (F.getName() != "main")
       continue;
 
-    
     Graph AbstractGraph = getAbstractGraph(F);
     getEdgeValues(F, AbstractGraph);
 
@@ -103,7 +132,6 @@ PreservedAnalyses BallLarusProfilerPass::run(Module& M,
         FunctionType::get(Type::getVoidTy(M.getContext()), Params, false);
     Function* sampleFun = Function::Create(
         fccType, GlobalValue::ExternalLinkage, "_Z7counteri", &M);
-
   }
   return PreservedAnalyses::none();
 }
