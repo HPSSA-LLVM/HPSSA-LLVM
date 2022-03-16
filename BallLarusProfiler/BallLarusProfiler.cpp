b4ff2ddd64387f292dc5ddde6233e99f4c321966
Mohd. Muzzammil <mohammadmzzmml@gmail.com>
2022-03-16 23:02:28 +0530

    WIP: BallLarus

--- BallLarusProfiler/BallLarusProfiler.cpp
+++ BallLarusProfiler/BallLarusProfiler.cpp
@@ -3,13 +3,73 @@
 using namespace llvm;
 using namespace std;
 
-void BallLarusProfilerPass::convertToDAG(Function& F) {
+map<std::pair<const BasicBlock*,const BasicBlock*>, bool> isBackedge;
+Graph BallLarusProfilerPass::getAbstractGraph(Function& F) {
+  BasicBlock* Exit = *po_begin(&F); // ? Exit will be the last block in post order iterator. Will it be inefficient?
+  Graph AbstractGraph;
   SmallVector<std::pair<const BasicBlock*, const BasicBlock*>> result;
   FindFunctionBackedges(F, result); // backedges in this function
+  map<std::pair<const BasicBlock*,const BasicBlock*>, bool> isBackedge;
+  for(auto [from, to]: result) {
+    isBackedge[make_pair(from, to)] = true;
+  }
+
+  uint backedge_ctr = 1;
+  for(auto& BB: F) {
+    for(auto Succ: successors(&BB)) {
+      if(!isBackedge[{&BB, Succ}]) {
+        Edge nedge;
+        nedge.from = &BB;
+        nedge.to = Succ;
+        nedge.backedge_number = 0; // ! not a backedge
+        AbstractGraph.G[&BB].push_back(nedge);
+      }
+      else {
+        Edge nedge1, nedge2;
+        nedge1.from = &F.getEntryBlock();
+        nedge1.to = Succ; // This will be the loop header
+        nedge1.backedge_number = backedge_ctr++;
+        nedge2.from = &BB;
+        nedge2.to = Exit; // Exit Block
+        nedge2.backedge_number = nedge1.backedge_number; // Correspond to the same backedge
+        AbstractGraph.G[nedge1.from].push_back(nedge1);
+        AbstractGraph.G[nedge2.from].push_back(nedge2);
+      }
+    }
+  }
+  return AbstractGraph;
+}
+void DFS(BasicBlock* BB)
+void DFSHelper() {
+
+}
+void BallLarusProfiler::getEdgeValues(Function &F, Graph &AG) {
+  map<BasicBlock*, int32_t> NumPaths;
+
+  for (auto it = po_begin(&F); it != po_end(&F);
+        ++it) { // Post Order Traversal--Reverse Topologiacl
+    BasicBlock* BB = *it;
+    if (succ_empty(BB)) { // Leaf Node
+      NumPaths[BB] = 1;
+    } else {
+      NumPaths[BB] = 0;
+      for (auto Succ : successors(BB)) {
+        if(!isBackedge[{BB, Succ}]) {
+          AG.Edges[{BB, Succ}] // think what to do??
+        }
+        NumPaths[BB] = NumPaths[BB] + NumPaths[Succ];
+      }
+    }
+  }
 }
 
+// void BallLarusProfilerPass::convertToDAG(Function& F) {
+  // SmallVector<std::pair<const BasicBlock*, const BasicBlock*>> result;
+  // FindFunctionBackedges(F, result); // backedges in this function
+// }
+
 void BallLarusProfilerPass::getAnalysisUsage(AnalysisUsage& Info) {
-  Info.addRequired<UnifyFunctionExitNodesLegacyPass>();
+  Info.addRequired<UnifyFunctionExitNodesLegacyPass>(); // This is required as the paper requires exactly one exit block
   // Used legacy pass for now, will change to new pass
 }
 
@@ -25,7 +85,9 @@ PreservedAnalyses BallLarusProfilerPass::run(Module& M,
     if (F.getName() != "main")
       continue;
 
-    convertToDAG(F);
+    
+    Graph AbstractGraph = getAbstractGraph(F);
+    getEdgeValues(F, AbstractGraph);
 
     // Inside main function
     IRBuilder<> Builder(M.getContext());
@@ -42,38 +104,6 @@ PreservedAnalyses BallLarusProfilerPass::run(Module& M,
     Function* sampleFun = Function::Create(
         fccType, GlobalValue::ExternalLinkage, "_Z7counteri", &M);
 
-    map<BasicBlock*, int32_t> NumPaths;
-
-    for (auto it = po_begin(&F); it != po_end(&F);
-         ++it) { // Post Order Traversal--Reverse Topologiacl
-      BasicBlock* BB = *it;
-      if (succ_empty(BB)) { // Leaf Node
-        NumPaths[BB] = 1;
-        std::vector<Value*> Args;
-        Builder.SetInsertPoint(BB->getTerminator());
-        Value* load =
-            Builder.CreateLoad(Type::getInt32Ty(BB->getContext()), gVar);
-        Args.push_back(load);
-        // inserting in the end of the basic block
-        CallInst::Create(sampleFun, Args, "", BB->getTerminator());
-      } else {
-        NumPaths[BB] = 0;
-        for (auto Succ : successors(BB)) {
-          BasicBlock* from = const_cast<BasicBlock*>(BB);
-          BasicBlock* to = const_cast<BasicBlock*>(
-              Succ); // ! Will this cast cause error in future?
-          BasicBlock* instrument = SplitEdge(from, to);
-          Builder.SetInsertPoint(instrument->getTerminator());
-          Value* load = Builder.CreateLoad(
-              Type::getInt32Ty(instrument->getContext()), gVar);
-          Value* newInst =
-              Builder.CreateAdd(load, Builder.getInt32(NumPaths[BB]));
-          Value* store = Builder.CreateStore(newInst, gVar);
-
-          NumPaths[BB] = NumPaths[BB] + NumPaths[Succ];
-        }
-      }
-    }
   }
   return PreservedAnalyses::none();
 }
